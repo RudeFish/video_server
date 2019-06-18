2f3922cd17b80450c30f398a76c4bed6d84dd509
RudeFish <llw9830@foxmail.com>
2019-06-19 06:52:48 +0800

    Update conn.go

--- api/dbops/conn.go
+++ api/dbops/conn.go
@@ -9,7 +9,7 @@ var (
 
 // 初始化数据库连接
 func init()  {
-	dbConn,err = sql.Open("mysql", "root:9830@tcp(localhost:3306)/video_server?charset-utf8")
+	dbConn,err = sql.Open("mysql", "username:pwd@tcp(ip:port)/dbname?charset-utf8")
 	if err != nil{
 		panic(err.Error())
 	}
